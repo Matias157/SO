a9fb785ee75d9ba77e06153f1fb063686ad17686
Matias157 <alexandrematias@alunos.utfpr.edu.br>
2019-04-30 20:55:42 -0300

    lab 03 iniciado

--- p03/pingpong.c
+++ p03/pingpong.c
@@ -2,11 +2,26 @@
 #include <stdlib.h>
 #include <ucontext.h>
 #include "pingpong.h"
+#include "queue.h"
 
 #define STACKSIZE 32768
 
 int cont = 1;
-task_t MainTask, *TaskCurrent, *TaskOld;
+task_t MainTask, *TaskCurrent, *TaskOld, *SuspendQueue, *ReadyQueue, Dispatcher;
+
+task_t *scheduler(){
+	return((task_t*)queue_remove((queue_t**)&ReadyQueue, (queue_t*)ReadyQueue)); //retorna o primeiro elemento da fila de tarefas
+}
+
+void dispatcher_body(){
+	while(queue_size((queue_t*) ReadyQueue) > 0){
+		task_t *next = scheduler();
+		if(next){
+			task_switch(next);
+		}
+	}
+	task_exit(0);
+}
 
 void pingpong_init (){
 	getcontext(&MainTask.context);
@@ -14,6 +29,7 @@ void pingpong_init (){
 	MainTask.id = 0;
 	MainTask.next = NULL;
 	TaskCurrent = &MainTask;
+	task_create(&Dispatcher, dispatcher_body, "");
 
 	setvbuf(stdout , 0, _IONBF, 0);
 }
@@ -38,7 +54,9 @@ int task_create (task_t *task, void (*start_func)(void *), void *arg){
 		exit (1);
 	}
 
-	makecontext (&(task->context), (void*)(*start_func), 1, arg);
+	queue_append((queue_t**)&ReadyQueue,(queue_t*)task);
+
+	makecontext(&(task->context), (void*)(*start_func), 1, arg);
 
 	task->id = cont;
 	cont++;
@@ -47,10 +65,10 @@ int task_create (task_t *task, void (*start_func)(void *), void *arg){
 }
 
 int task_switch (task_t *task){
-		TaskOld = TaskCurrent;
-		TaskCurrent = task;
-		swapcontext(&(TaskOld->context), &(TaskCurrent->context));
-		return(0);
+	TaskOld = TaskCurrent;
+	TaskCurrent = task;
+	swapcontext(&(TaskOld->context), &(TaskCurrent->context));
+	return(0);
 }
 
 void task_exit (int exitCode){
@@ -59,4 +77,8 @@ void task_exit (int exitCode){
 
 int task_id (){
 	return(TaskCurrent->id);
+}
+
+void task_yield (){
+	task_switch(&Dispatcher);
 }
