7227284e0d55ff58cbdd82c9c6df6e8795530505
Matias157 <alexandrematias@alunos.utfpr.edu.br>
2019-05-05 16:22:21 -0300

    Lab05 finalizado

--- p05/pingpong.c
+++ p05/pingpong.c
@@ -1,65 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ucontext.h>
+#include <signal.h>
+#include <sys/time.h>
 #include "pingpong.h"
 #include "queue.h"
 
 #define STACKSIZE 32768
 
+// estrutura que define um tratador de sinal (deve ser global ou static)
+struct sigaction action ;
+
+// estrutura de inicialização to timer
+struct itimerval timer;
+
 int cont = 1;
+int ticks = 0;
 task_t MainTask, *TaskCurrent, *TaskOld, *SuspendQueue, *ReadyQueue, Dispatcher;
 
-task_t *scheduler(){
-	task_t *task_mais_prioritaria = ReadyQueue, *task_comparacao = ReadyQueue->next; // a task mais prioritaria é a com menor prioridade
-	while(task_comparacao != ReadyQueue){											// avalia até voltar a cabeça da fila
-		//printf("%d %d\n", task_mais_prioritaria->prio_dim, task_comparacao-> prio_dim);
-		if(task_mais_prioritaria->prio_dim >= task_comparacao->prio_dim){			// se a task mais prioritaria até então tiver prioridade menor
-			if(task_mais_prioritaria->prio_dim > -20){								// e se a task mais prioritaria não tiver prioridade menor que -20
-				task_mais_prioritaria->prio_dim--;									// envenhece a prioridade da task mais prioritaria
-			}
-			task_mais_prioritaria = task_comparacao;								// a task mais prioritária pasa a ser a avaliada
-		}
-		else{																		// se a task mais prioritária tiver ainda a maior prioridade
-			if(task_comparacao->prio_dim > -20){										// e se não tiver prioridade menor que -20	
-				task_comparacao->prio_dim--;										// a task compara envelhece		
-			}
-		}
-		task_comparacao = task_comparacao->next;									// compara com a task na sequencia
+void timer_handler(){ //tratador do timer
+	if(ticks <= 20)
+		ticks++;
+	else{
+		ticks = 0;
+		task_yield();
 	}
-	return((task_t*)queue_remove((queue_t**)&ReadyQueue, (queue_t*)task_mais_prioritaria));	// retorna a task mais prioritária	
+}
+
+task_t *scheduler(){
+	return((task_t*)queue_remove((queue_t**)&ReadyQueue, (queue_t*)ReadyQueue));
 }
 
 void dispatcher_body(){
 	while(queue_size((queue_t*) ReadyQueue) > 0){
 		task_t *next = scheduler();
 		if(next){
+			if(setitimer (ITIMER_REAL, &timer, 0) < 0){ //arma o temporizador ITIMER_REAL
+            	perror ("Erro em setitimer: ") ;
+            	exit (1) ;
+            }
 			task_switch(next);
 		}
 	}
 	task_exit(0);
-}
-
-void task_setprio (task_t *task, int prio){
-	if(prio > 20 || prio < -20){
-		printf("Prioridade fora dos limites permitidos\n");
-	}
-	else{
-		if(task == NULL){
-			TaskCurrent->prio_est = prio;
-		}
-		else{
-			task->prio_est = prio;
-			task->prio_dim = prio;
-		}
-	}
-}
-
-int task_getprio (task_t *task){
-	task_t *aux = task;
-	if(task == NULL){
-		aux = TaskCurrent;
-	}
-	return(aux->prio_est);
 }
 
 void pingpong_init (){
@@ -69,7 +52,17 @@ void pingpong_init (){
 	MainTask.next = NULL;
 	MainTask.status = Running;
 	TaskCurrent = &MainTask;
-	task_create(&Dispatcher, dispatcher_body, "");
+	action.sa_handler = timer_handler;
+    sigemptyset (&action.sa_mask);
+    action.sa_flags = 0;
+    if(sigaction (SIGALRM, &action, 0) < 0){
+    	perror ("Erro em sigaction: ");
+    	exit (1);
+    }
+    // ajusta valores do temporizador
+    timer.it_value.tv_usec = 1000;      // primeiro disparo, em micro-segundos
+    timer.it_interval.tv_usec = 1000;   // disparos subsequentes, em micro-segundos
+    task_create(&Dispatcher, dispatcher_body, "");
 
 	setvbuf(stdout , 0, _IONBF, 0);
 }
@@ -94,15 +87,13 @@ int task_create (task_t *task, void (*start_func)(void *), void *arg){
 		exit (1);
 	}
 
-	if(task != &Dispatcher){
+	if(task != &Dispatcher)
 		queue_append((queue_t**)&ReadyQueue,(queue_t*)task);
-	}
 
 	makecontext(&(task->context), (void*)(*start_func), 1, arg);
 
 	task->status = Ready;
-	task->prio_est = 0;
-	task->prio_dim = 0;
+
 	task->id = cont;
 	cont++;
 
@@ -118,12 +109,10 @@ int task_switch (task_t *task){
 }
 
 void task_exit (int exitCode){
-	if(TaskCurrent == &Dispatcher){
+	if(TaskCurrent == &Dispatcher)
 		task_switch(&MainTask);
-	}
-	else{
+	else
 		task_switch(&Dispatcher);
-	}
 }
 
 int task_id (){
@@ -131,12 +120,9 @@ int task_id (){
 }
 
 void task_yield (){
-	if(TaskCurrent != &MainTask){
-		//ver se coloca para voltar à prioridade original aqui ou não
-		TaskCurrent->prio_dim = TaskCurrent->prio_est; // acho que sim
+	if(TaskCurrent != &MainTask)
 		queue_append((queue_t**)&ReadyQueue,(queue_t*)TaskCurrent);
 		TaskCurrent->status = Ready;
-	}
 	task_switch(&Dispatcher);
 }
 
